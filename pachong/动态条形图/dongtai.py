1fe6e9c6bb1ef964e7036dd8601049c69692a32a
zhaogev5 <1226844358@qq.com>
2020-07-03 15:51:35 +0800

    lyz

---
+++ pachong/动态条形图/dongtai.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+
+a = 1
+ab = [1,2,3]
+fpp = "hello"
+
+for i in range(6):
+    a += 1
+
+
